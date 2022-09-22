7f32c74e489ad9e0c6ab74ff066b2cff284b4f49
Hassan Khan <hassankpisces@gmail.com>
2022-09-22 06:35:12 +0500

    Adding Exercise-4

---
+++ Exercise-4/Exercise-4/Program.cs
@@ -0,0 +1,129 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("\nData type: INT");
+// Declare 3 variables in int
+int originalFahr;
+int calcCelsius;
+int calcFahr;
+
+// Assign temperature 0 in int;
+originalFahr = 0;
+Console.WriteLine("\nOriginal: " + originalFahr);
+
+// Convert to Celsius
+calcCelsius = ((originalFahr - 32) / 9) * 5;
+Console.WriteLine("Calculated Celsius: " + calcCelsius);
+
+// Convert back to Fahrenheit 
+calcFahr = (((calcCelsius * 9) / 5) + 32);
+Console.WriteLine("Calculated Fahrenheit: " + calcFahr);
+
+// Assign temperature 32 in int;
+originalFahr = 32;
+Console.WriteLine("\nOriginal: " + +originalFahr);
+
+// Convert to Celsius
+calcCelsius = ((originalFahr - 32) / 9) * 5;
+Console.WriteLine("Calculated Celsius: " + calcCelsius);
+
+// Convert back to Fahrenheit 
+calcFahr = (((calcCelsius * 9) / 5) + 32);
+Console.WriteLine("Calculated Fahrenheit: " + calcFahr);
+
+// Assign temperature 212 in int;
+originalFahr = 212;
+Console.WriteLine("\nOriginal: " + originalFahr);
+
+// Convert to Celsius
+calcCelsius = ((originalFahr - 32) / 9) * 5;
+Console.WriteLine("Calculated Celsius: " + calcCelsius);
+
+// Convert back to Fahrenheit 
+calcFahr = (((calcCelsius * 9) / 5) + 32);
+Console.WriteLine("Calculated Fahrenheit: " + calcFahr);
+
+Console.WriteLine("\nData type: FLOAT");
+
+// Declare 3 variables in float
+float floatOriginalFahr;
+float floatCalcCelsius;
+float floatCalcFahr;
+
+// Assign temperature 0 in int;
+floatOriginalFahr = 0;
+Console.WriteLine("\nOriginal: " + floatOriginalFahr);
+
+// Convert to Celsius
+floatCalcCelsius = ((floatOriginalFahr - 32) / 9) * 5;
+Console.WriteLine("Calculated Celsius: " + floatCalcCelsius);
+
+// Convert back to Fahrenheit 
+floatCalcFahr = (((floatCalcCelsius * 9) / 5) + 32);
+Console.WriteLine("Calculated Fahrenheit: " + floatCalcFahr);
+
+// Assign temperature 32 in int;
+floatOriginalFahr = 32;
+Console.WriteLine("\nOriginal: " + floatOriginalFahr);
+
+// Convert to Celsius
+floatCalcCelsius = ((floatOriginalFahr - 32) / 9) * 5;
+Console.WriteLine("Calculated Celsius: " + floatCalcCelsius);
+
+// Convert back to Fahrenheit 
+floatCalcFahr = (((floatCalcCelsius * 9) / 5) + 32);
+Console.WriteLine("Calculated Fahrenheit: " + floatCalcFahr);
+
+// Assign temperature 212 in int;
+floatOriginalFahr = 212;
+Console.WriteLine("\nOriginal: " + floatOriginalFahr);
+
+// Convert to Celsius
+floatCalcCelsius = ((floatOriginalFahr - 32) / 9) * 5;
+Console.WriteLine("Calculated Celsius: " + floatCalcCelsius);
+
+// Convert back to Fahrenheit 
+floatCalcFahr = (((floatCalcCelsius * 9) / 5) + 32);
+Console.WriteLine("Calculated Fahrenheit: " + floatCalcFahr);
+
+Console.WriteLine("\nData type: DOUBLE");
+
+// Declare 3 variables in double
+double doubleOriginalFahr;
+double doubleCalcCelsius;
+double doubleCalcFahr;
+
+// Assign temperature 0 in int;
+doubleOriginalFahr = 0;
+Console.WriteLine("\nOriginal: " + doubleOriginalFahr);
+
+// Convert to Celsius
+doubleCalcCelsius = ((doubleOriginalFahr - 32) / 9) * 5;
+Console.WriteLine("Calculated Celsius: " + doubleCalcCelsius);
+
+// Convert back to Fahrenheit 
+doubleCalcFahr = (((doubleCalcCelsius * 9) / 5) + 32);
+Console.WriteLine("Calculated Fahrenheit: " + doubleCalcFahr);
+
+// Assign temperature 32 in int;
+doubleOriginalFahr = 32;
+Console.WriteLine("\nOriginal: " + doubleOriginalFahr);
+
+// Convert to Celsius
+doubleCalcCelsius = ((doubleOriginalFahr - 32) / 9) * 5;
+Console.WriteLine("Calculated Celsius: " + doubleCalcCelsius);
+
+// Convert back to Fahrenheit 
+doubleCalcFahr = (((doubleCalcCelsius * 9) / 5) + 32);
+Console.WriteLine("Calculated Fahrenheit: " + doubleCalcFahr);
+
+// Assign temperature 212 in int;
+doubleOriginalFahr = 212;
+Console.WriteLine("\nOriginal: " + doubleOriginalFahr);
+
+// Convert to Celsius
+doubleCalcCelsius = ((doubleOriginalFahr - 32) / 9) * 5;
+Console.WriteLine("Calculated Celsius: " + doubleCalcCelsius);
+
+// Convert back to Fahrenheit 
+doubleCalcFahr = (((doubleCalcCelsius * 9) / 5) + 32);
+Console.WriteLine("Calculated Fahrenheit: " + doubleCalcFahr);
