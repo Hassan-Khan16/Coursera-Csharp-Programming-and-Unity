6dab78dd9ceea87c633c92615f4f05a6d8a5d4cb
Hassan Khan <hassankpisces@gmail.com>
2022-09-22 05:37:05 +0500

    Debug exercise manually added using cmd

---
+++ DebugExercise/DebugExercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DebugExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float angle;
+
+            Console.WriteLine("Enter angle in degrees: ");
+            angle = float.Parse(Console.ReadLine());
+
+            angle *= MathF.PI / 180;
+
+            Console.WriteLine(MathF.Cos(angle));
+            Console.WriteLine(MathF.Sin(angle));
+        }
+    }
+}
