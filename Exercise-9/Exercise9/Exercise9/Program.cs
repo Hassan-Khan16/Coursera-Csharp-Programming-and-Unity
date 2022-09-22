6293968b0e5fb4e7358264fcb711bb1dc84d4983
Hassan Khan <hassankpisces@gmail.com>
2022-09-22 06:08:42 +0500

    Adding exercise 9 manually

---
+++ Exercise-9/Exercise9/Exercise9/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exercise9
+{
+    /// <summary>
+    /// Exercise 9 solution 
+    /// </summary>
+
+    internal class Program
+    {
+        /// <summary>
+        /// Demonstrates using custom classes
+        /// </summary>
+        /// <param name="args">command-line arguments</param>
+        static void Main(string[] args)
+        {
+            // create a new deck and tell the deck to print itself
+            Deck deck = new Deck();
+            deck.Print();
+            Console.WriteLine();
+
+            // tell the deck to shuffle and print itself
+            deck.Shuffle();
+            deck.Print();
+
+            // take the top card from the deck and print the card rank and suit
+            Card card1 = deck.TakeTopCard();
+            Console.WriteLine();
+            Console.WriteLine("First card: " + card1.Rank + " of " + card1.Suit);
+
+            // take the top card from the deck and print the card rank and suit
+            Card card2 = deck.TakeTopCard();
+            Console.WriteLine("Second card: "+ card2.Rank+" of "+ card2.Suit);
+        }
+    }
+}
